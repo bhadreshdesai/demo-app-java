86e4a4a3766607d5f65fc2812cc1f92644c2ffac
bhadreshdesai <bhadresh_desai@hotmail.com>
2021-03-13 14:40:12 +0000

    Debug and Swagger URL Fix
    Use vscjava.vscode-java-debug@0.28.0 in .gitpod.yml
    Use server.forward-headers-strategy=framework

--- src/main/java/bdd/demo/appjava/DemoApplication.java
+++ src/main/java/bdd/demo/appjava/DemoApplication.java
@@ -22,17 +22,16 @@ public class DemoApplication {
     //public OpenAPI openAPI() {
     //    return new OpenAPI().addServersItem(new Server().url("/"));
     //}
-
     @Bean
 	public OpenApiCustomiser serverOpenApiCustomiser1() {
 		return openAPI -> openAPI.getServers().forEach(server -> {
             server.setDescription("Demo Java Application");
+            // Set server.forward-headers-strategy=framework in application.properties
             // TODO: Find a better way to get https:// instead of http://
             // https://github.com/springdoc/springdoc-openapi/issues/751#issuecomment-650734224
-            String url = server.getUrl();
-            url = url.replaceAll("http://", "https://");
-            server.setUrl(url);
+            //String url = server.getUrl();
+            //url = url.replaceAll("http://", "https://");
+            //server.setUrl(url);
         });
 	}
-
 }
